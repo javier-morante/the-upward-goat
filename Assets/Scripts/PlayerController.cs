5f80d24ca3d9f32a5a6bfd539bb62064a2b1b9ec
Javier.Morante <javichu.monu@gmail.com>
2024-05-17 19:08:16 +0200

    Added animations and basic state machine for player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,7 @@ using UnityEditor.Tilemaps;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
-public class Player : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5;
     [SerializeField] private float jumpValue = 0;
@@ -14,20 +14,29 @@ public class Player : MonoBehaviour
     [SerializeField] private float jumpPerF = 0.1f;
     [SerializeField] private LayerMask goundMarsk;
     [SerializeField] private Vector2 boxSize;
+    [SerializeField] private ParticleSystem jumpDust;
+    [SerializeField] private ParticleSystem stompP;
     [SerializeField] private float castDistance;
     [SerializeField] private float maxFall;
     [SerializeField] private PhysicsMaterial2D normalMat;
     [SerializeField] private PhysicsMaterial2D bounceMat;
     [SerializeField] private float moveDesaceleration = 1;
     [SerializeField] private float jumpMoveSpeed = 7f;
-    [SerializeField] private ParticleSystem jumpDust;
-    
+    [SerializeField] private float maxfallToStomp = 5;
 
+    private enum State
+    {
+        Idle,
+        Running,
+        Jumping,
+        Falling,
+        Stomping
+    }
+    
+    private float? fallMoment = null;
     private BoxCollider2D _col;
     private bool _jumpPreesed;
     private float _lateralMove;
-    private float screenWidth;
-    private float screenHeight;
     private Rigidbody2D _rb;
     private bool _isGrounded;
 
@@ -35,24 +44,11 @@ public class Player : MonoBehaviour
         _rb = GetComponent<Rigidbody2D>();
         _col = GetComponent<BoxCollider2D>();
     }
-    // void Start()
-    // {
-    //     _rb = GetComponent<Rigidbody2D>();
-    //     _col = GetComponent<BoxCollider2D>();
-    // }
 
-    // Update is called once per frame
     void Update()
     {
         _jumpPreesed = Input.GetButton("Jump");
         _lateralMove = Input.GetAxisRaw("Horizontal");
-
-        screenWidth = (Camera.main.orthographicSize * Screen.width / Screen.height) * 2;
-        screenHeight = Camera.main.orthographicSize * 2;
-        if (transform.position.x < Camera.main.transform.position.x - screenWidth / 2)
-        {
-            Debug.Log("out of camara");
-        }
     }
 
     void FixedUpdate()
@@ -62,7 +58,30 @@ public class Player : MonoBehaviour
             _rb.velocity = new Vector2(Mathf.MoveTowards(_rb.velocity.x,0,moveDesaceleration*Time.fixedDeltaTime),Mathf.Max(_rb.velocity.y,-maxFall));
         }
         Jump();
+        Stomp();
         Move();
+    }
+
+    private void Stomp()
+    {
+        Vector2 velocitat = _rb.velocity;
+        if(!_isGrounded && velocitat.y < 0 && fallMoment == null){
+
+                fallMoment = transform.position.y;
+                
+        }
+
+        if(_isGrounded && fallMoment != null){
+
+            float distance = Vector2.Distance(new Vector2(0,fallMoment.Value),new Vector2(0,transform.position.y));
+            
+            if( distance > maxfallToStomp){
+                stompP.Play();
+            }
+
+            fallMoment = null;
+        }
+
     }
 
     void Jump()
@@ -74,6 +93,7 @@ public class Player : MonoBehaviour
             jumpValue += jumpPerF;
             
         }
+
 
 
         if ((jumpValue >= maxJump || !_jumpPreesed && jumpValue >= 0.1f) && _isGrounded)
